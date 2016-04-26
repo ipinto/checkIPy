04ed8584713b624356915186a51ba9e8bdeda831
Imanol Pinto <ipintoc@gmail.com>
2016-04-26 23:05:08 +0200

    Add argument to print or send an email with the IP.

--- checkIP.py
+++ checkIP.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import argparse
 import smtplib
 import urllib2
 import settings
 
-def send_email():
-    message = """\
-    From: {from_address}
-    To: {to_address}
-    Subject: {subject}
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Check your external IP.')
+    parser.add_argument('-e', '--email', dest='action', action='store_const',
+                   const=send_email, default=print_ip,
+                   help='send an email with your IP (default: print it on terminal)')
+    return parser.parse_args()
 
-    {message}
+def print_ip(ip):
+    print "External IP: {}".format(ip)
+
+def send_email(ip):
+    message = """\
+From: {from_address}
+To: {to_address}
+Subject: {subject}
+
+{message}
     """.format(from_address = settings.FROM,
                 to_address = settings.TO,
                 subject = 'Your IP status',
-                message = 'Your IP is: {}'.format(urllib2.urlopen(settings.IP_SOURCE).read()))
+                message = 'Your IP is: {}'.format(ip))
 
     # Send email
     server = smtplib.SMTP(settings.SMTP)
@@ -24,4 +35,5 @@ def send_email():
     server.quit()
 
 if __name__ == "__main__":
-    send_email()
+    args = get_arguments()
+    args.action(urllib2.urlopen(settings.IP_SOURCE).read())
