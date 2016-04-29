3c8920715f7c5e36b06c414a12930849867d099f
Imanol Pinto <ipintoc@gmail.com>
2016-04-29 19:22:25 +0200

    Add telegram bot.
    I still need to figure out which is the best way to get the chat_id and store it.

--- checkIPy.py
+++ checkIPy.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import argparse
-import smtplib
-import urllib2
 import settings
+import smtplib
+import telegram
+import urllib2
+
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description='Check your external IP.')
-    parser.add_argument('-e', '--email', dest='action', action='store_const',
-                   const=send_email, default=print_ip,
-                   help='send an email with your IP (default: print it on terminal)')
+    parser = argparse.ArgumentParser(description = 'Check your external IP.')
+    parser.add_argument('-c', '--console', action='store_true',
+                   help = 'print your IP on console (default action)')
+    parser.add_argument('-e', '--email', action='store_true',
+                   help = 'send an email with your IP')
+    parser.add_argument('-t', '--telegram', action='store_true',
+                   help = 'send a Telegram message with your IP')
     return parser.parse_args()
 
 def print_ip(ip):
-    print "External IP: {}".format(ip)
+    print ip
 
 def send_email(ip):
     message = """\
@@ -34,6 +39,14 @@ Subject: {subject}
     server.sendmail(settings.FROM, settings.TO, message)
     server.quit()
 
+def send_telegram_message(ip):
+    bot = telegram.Bot(token = settings.TELEGRAM_TOKEN)
+    bot.sendMessage(chat_id = settings.TELEGRAM_CHAT_ID,
+        text = "Your IP is: {}".format(ip))
+
 if __name__ == "__main__":
+    ip = urllib2.urlopen(settings.IP_SOURCE).read()
     args = get_arguments()
-    args.action(urllib2.urlopen(settings.IP_SOURCE).read())
+    if args.email: send_email(ip)
+    if args.telegram: send_telegram_message(ip)
+    if args.console or (not args.email and not args.telegram): print_ip(ip)
--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 # SMTP Config
 SMTP='smtp.gmail.com:587'
 FROM='from_email@gmail.com'
@@ -9,3 +14,8 @@ TO='to_email@gmail.com'
 
 # IP endpoint
 IP_SOURCE='https://api.ipify.org'
+
+# Telegram Bot
+TELEGRAM_TOKEN='your_bot_token'
+# chatId = bot.getUpdates()[-1].message.chat_id
+TELEGRAM_CHAT_ID='your_chat_id'
